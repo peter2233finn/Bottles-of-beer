2f4db7e2bd7fd6f49db928b4f82e08af972a897f
Peter Finn <c16407722@mydit.ie>
2020-12-22 17:13:47 +0000

    Update annoying.sh

--- annoying.sh
+++ annoying.sh
@@ -1,7 +1,7 @@
 i=1000000
 while [ $i != -1 ]
 do
-	espeak -s 120 "$i bottels of beer on the wall.... $i bottels of bear.... you take one down, pass it around...."
+	espeak -s 120 "$i bottels of beer on the wall.... $i bottels of beer.... you take one down, pass it around...."
 	((i--))
 	espeak -s 120 "$i bottels of beer on the wall......"
 	sleep 1
